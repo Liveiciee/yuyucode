c2281b21a00126a190aa7bfc878b0f2d12f1d760
liveiciee <liveiciee@gmail.com>
2026-03-16 06:20:22 +0800

    fix: enable WebGPU di Capacitor WebView

--- android/app/src/main/java/com/liveiciee/yuyulife/MainActivity.java
+++ android/app/src/main/java/com/liveiciee/yuyulife/MainActivity.java
@@ -1,5 +1,17 @@
 package com.liveiciee.yuyulife;
 
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        WebView.setWebContentsDebuggingEnabled(true);
+    }
+}
