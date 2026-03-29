03b6904fcbb2aaa60c68007bd5127827b26a7477
Liveiciee <liveiciee@gmail.com>
2026-03-30 04:43:22 +0800

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,13 +1,13 @@
 // src/utils.js - Entry point re-exporting modular utilities
 export { logger } from './utils/logger.js';
-export { countTokens } from './utils/token.js';
+export { countTokens, countTokensBreakdown, estimateContextUsage, trimToTokenBudget } from './utils/token.js';
 export { getFileIcon } from './utils/icon.js';
 export { hl } from './utils/highlight.js';
 export { resolvePath } from './utils/path.js';
-export { parseActions } from './utils/actionParser.js';
-export { generateDiff } from './utils/diff.js';
+export { parseActions, parseActionsWithMeta } from './utils/actionParser.js';
+export { generateDiff, executeDiff } from './utils/diff.js';
 export { executeAction } from './utils/actionExecutor.js';
 export { getSyntaxCmd, verifySyntaxBatch } from './utils/syntax.js';
 export { backupFiles } from './utils/backup.js';
-export { sleep, getBackoffDelay, isRetryableError, isRetryableStatus } from './utils/retry.js';
+export { sleep, getBackoffDelay, isRetryableError, isRetryableStatus, withRetry, withRetryOnStatus } from './utils/retry.js';
 export { injectVisionImage } from './utils/vision.js';
